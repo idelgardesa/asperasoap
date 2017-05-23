8b3f6bf5382c0dc2291f61cb44bc9512d0244c7f
eliene <elienemb@gmail.com>
2017-05-23 23:32:42 +0200

    maria

---
+++ asperasoap/src/main/java/com/avioon/Maria.java
@@ -0,0 +1,21 @@
+package com.avioon;
+
+import aspera.central._2012._10.transfer201210.FilterChangedErrorFault;
+import aspera.central._2012._10.transfer201210.JobFormatInvalidFault;
+import aspera.central._2012._10.transfer201210.JobSubmissionErrorFault;
+import aspera.central._2012._10.transfer201210.JobTypeNotFoundFault;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class Maria 
+{
+    public static void main( String[] args )
+    {
+                System.out.println("worked without exception");
+    }
+}
